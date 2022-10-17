92705df00d75ab994bde31031a49d19c6db6fe22
Abocha <AMagomedov2@alfabank.ru>
2022-10-17 15:31:25 +0300

    init commit
    (cherry picked from commit 1069134c3849ae02df624bbade8ee65015b8705f)

---
+++ app/src/main/java/com/abocha/homeworkfragments/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.abocha.homeworkfragments
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
